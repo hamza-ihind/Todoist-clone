14a7b585451b0670e34f8abe111aef466571f45f
E11even <ihind.mizo@gmail.com>
2021-10-20 15:23:42 +0100

    set up project files and getting ready to make Firebase

---
+++ todoist-clone/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "./components/layout/Header";
+import { Content } from "./components/layout/Content";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+    </div>
+  );
+};
+
+export default App;
