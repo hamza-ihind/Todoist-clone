db3266de1e7811c0aef39c10731b5786f88813a5
E11even <ihind.mizo@gmail.com>
2021-10-29 15:36:42 +0100

    done the styling secrion to the header and the navbar

--- todoist-clone/src/components/layout/Sidebar.jsx
+++ todoist-clone/src/components/layout/Sidebar.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
-import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa'
+import React from "react";
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
 
 export const Sidebar = () => (
-    <div className="sidebar" data-testid="sidebar">
-        <ul className='sidebar-generic'>
-            <li><span>< FaInbox /></span><span>Inbox</span></li>
-            <li><span>< FaRegCalendar /></span><span>Today</span></li>
-            <li><span>< FaRegCalendarAlt /></span><span>Next 7 Days</span></li>
-        </ul>
-        <div className="sidebar__middle">
-            <span>
-                <FaChevronDown/>
-            </span>
-            <h2>Projects</h2>
-        </div>
-        <ul className="sidebar__projects">
-            Projects will be Here
-        </ul>
+  <div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar-generic">
+      <li data-testid="inbox" className="inbox">
+        <span>
+          <FaInbox />
+        </span>
+        <span>Inbox</span>
+      </li>
+      <li data-testid="today" className="today">
+        <span>
+          <FaRegCalendar />
+        </span>
+        <span>Today</span>
+      </li>
+      <li data-testid="next_7" className="next_7">
+        <span>
+          <FaRegCalendarAlt />
+        </span>
+        <span>Next 7 Days</span>
+      </li>
+    </ul>
+    <div className="sidebar__middle">
+      <span>
+        <FaChevronDown />
+      </span>
+      <h2>Projects</h2>
     </div>
-)
-
+    <ul className="sidebar__projects">Projects will be Here</ul>
+  </div>
+);
